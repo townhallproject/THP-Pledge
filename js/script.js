195ff0bf0aa504f862452b90b841bdfde55f25d8
Paolo Unger Dvorchik <paolo@spry-group.com>
2018-04-30 20:14:43 -0400

    Add testing data and wire up state selector

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-$( document ).ready(()=> {
-  // Setup state select
-  var selectedState$ = Rx.Observable.fromEvent($('#select--state'), 'change');
-
-  selectedState$.subscribe(function(res) {
-    console.log(res);
-  });
-});
-
 var fullData;
 var filteredData$ = new Rx.BehaviorSubject();
 var dataByState;
+
+// Setup state selector
+$( document ).ready(()=> {
+  initStateSelector($('#select--state'));
+  var selectedState$ = Rx.Observable.fromEvent($('#select--state'), 'change');
+  selectedState$.subscribe(function(res) {
+    let key = res.currentTarget.value;
+    filteredData$.next(key ? {[key]: dataByState[key]} : dataByState);
+  });
+});
 
 // Seed initial data
 var initalData = $.ajax({ url: 'data/testData.json' }).then((res) => {
@@ -17,6 +18,7 @@ var initalData = $.ajax({ url: 'data/testData.json' }).then((res) => {
   dataByState = groupByState(res);
   filteredData$.next(dataByState);
 });
+
 
 // Helper functions
 function groupByState(data) {
@@ -28,4 +30,70 @@ function groupByState(data) {
     res[key].push(record);
     return res;
   }, {});
+}
+
+function initStateSelector(ele) {
+  Object.keys(stateDict).forEach(key => {
+    ele.append('<option value="' + key + '">' + stateDict[key] + '</option>');
+  });
+}
+
+// Data
+var stateDict = {
+  "AL": "Alabama",
+  "AK": "Alaska",
+  "AS": "American Samoa",
+  "AZ": "Arizona",
+  "AR": "Arkansas",
+  "CA": "California",
+  "CO": "Colorado",
+  "CT": "Connecticut",
+  "DE": "Delaware",
+  "DC": "District Of Columbia",
+  "FL": "Florida",
+  "GA": "Georgia",
+  "GU": "Guam",
+  "HI": "Hawaii",
+  "ID": "Idaho",
+  "IL": "Illinois",
+  "IN": "Indiana",
+  "IA": "Iowa",
+  "KS": "Kansas",
+  "KY": "Kentucky",
+  "LA": "Louisiana",
+  "ME": "Maine",
+  "MD": "Maryland",
+  "MA": "Massachusetts",
+  "MI": "Michigan",
+  "MN": "Minnesota",
+  "MS": "Mississippi",
+  "MO": "Missouri",
+  "MT": "Montana",
+  "NE": "Nebraska",
+  "NV": "Nevada",
+  "NH": "New Hampshire",
+  "NJ": "New Jersey",
+  "NM": "New Mexico",
+  "NY": "New York",
+  "NC": "North Carolina",
+  "ND": "North Dakota",
+  "MP": "Northern Mariana Islands",
+  "OH": "Ohio",
+  "OK": "Oklahoma",
+  "OR": "Oregon",
+  "PA": "Pennsylvania",
+  "PR": "Puerto Rico",
+  "RI": "Rhode Island",
+  "SC": "South Carolina",
+  "SD": "South Dakota",
+  "TN": "Tennessee",
+  "TX": "Texas",
+  "UT": "Utah",
+  "VT": "Vermont",
+  "VI": "Virgin Islands",
+  "VA": "Virginia",
+  "WA": "Washington",
+  "WV": "West Virginia",
+  "WI": "Wisconsin",
+  "WY": "Wyoming",
 }
--- js/table.js
+++ js/table.js
@@ -4,15 +4,27 @@ $table = $('#table--state');
 
 filteredData$.subscribe(data => {
   $table.empty();
-  Object.keys(data).forEach(key => addRow(key, data[key]));
+  let length = Object.keys(data).length;
+  if ( length > 1) {
+    Object.keys(data).forEach(key => {
+      $table.append('<div class="row"><div class="col-12"><h4>' + stateDict[key] + '</h4></div></div>'); 
+      addRow(data[key])
+    });
+  } else if (length === 1) {
+    if (data[Object.keys(data)[0]]) {
+      // Don't bother with headers if only one state is shown
+      addRow(data[Object.keys(data)[0]]);
+    } else {
+      $table.append('<div class="row"><div class="col-12"><h4>No data for ' + stateDict[Object.keys(data)[0]] + '</h4></div></div>'); 
+    }
+  }
 });
 
-function addRow(key, arr) {
-  let row = '<div class="row"><div class="col-12">' + key + '</div></div>';
+function addRow(arr) {
   arr.forEach(record => {
-    row += '<div class="col-3">' + record.firstName + ' ' + record.lastName + '</div>';
+    $table.append('<div class="col-3">' + record.name + '</div>');
   });
-  $table.append(row);
+  
 }
 
 });
