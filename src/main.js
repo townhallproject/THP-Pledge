c818f97786f26a10a57e0c2e038d013f663163af
meganrm <meganrm@gmail.com>
2018-05-19 02:58:45 -0700

    pulled out main components to an App component

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
-import Nav from './components/Nav';
-import Header from './components/Header';
-import CounterBar from './components/CounterBar';
+import App from './App';
 
-// import configureStore from './store/configureStore';
+import configureStore from './store/configureStore';
 
 import './style/app.scss';
 
-// const store = configureStore();
+const store = configureStore();
 
 const jsx = (
-  // <Provider store={store}>
-  <div>
-    <Nav />
-    <Header />
-    <CounterBar />
-  </div>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 const renderApp = () => {
