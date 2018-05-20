2421bb04955d26c9035702258eb982267e83167b
meganrm <meganrm@gmail.com>
2018-05-20 00:19:01 -0700

    added popover for counter

--- src/components/CounterBar.js
+++ src/components/CounterBar.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Popover } from 'antd';
 
 import { totalIncumbentsForParty } from './utils';
+
+/* eslint-disable */
+require('style-loader!css-loader!antd/es/popover/style/index.css');
+/* eslint-enable */
 
 export default (props) => {
   const totalDReps = (totalIncumbentsForParty(props.allPledgers, 'D', true));
@@ -22,12 +27,24 @@ export default (props) => {
   return (
     <div className="container-fluid">
       <div className="row header-pledge--count">
-        <div style={{ flexBasis: `${dRepsPercent}%` }} className="democrat" title="Democratic Representatives">{totalDReps}</div>
-        <div style={{ flexBasis: `${dCandidatePercent}%` }} className="democrat-candidate" title="Democratic Candidates">{totalDCandidates}</div>
-        <div style={{ flexBasis: `${iRepsPercent}%` }} className="independent" title="Independent Representatives">{totalIReps}</div>
-        <div style={{ flexBasis: `${iCandidatePercent}%` }} className="independent-candidate" title="Independent Candidates">{totalICandidates}</div>
-        <div style={{ flexBasis: `${rCandidatePercent}%` }} className="republican-candidate" title="Republican Candidates">{totalRCandidates}</div>
-        <div style={{ flexBasis: `${rRepsPercent}%` }} className="republican" title="Republican Representatives">{totalRReps}</div>
+        <Popover content="Pledged Democratic Representatives" placement="topLeft" >
+          <div style={{ flexBasis: `${dRepsPercent}%` }} className="democrat" >{totalDReps}</div>
+        </Popover>
+        <Popover content="Pledged Democratic Candidates" >
+          <div style={{ flexBasis: `${dCandidatePercent}%` }} className="democrat-candidate" title="Democratic Candidates">{totalDCandidates}</div>
+        </Popover>
+        <Popover content="Pledged Independent Representatives">
+          <div style={{ flexBasis: `${iRepsPercent}%` }} className="independent" title="Independent Representatives">{totalIReps}</div>
+        </Popover>
+        <Popover content="Pledged Independent Candidates">
+          <div style={{ flexBasis: `${iCandidatePercent}%` }} className="independent-candidate" title="Independent Candidates">{totalICandidates}</div>
+        </Popover>
+        <Popover content="Pledged Republican Candidates" placement="topRight">
+          <div style={{ flexBasis: `${rCandidatePercent}%` }} className="republican-candidate" title="Republican Candidates">{totalRCandidates}</div>
+        </Popover>
+        <Popover content="Pledged Republican Representatives" placement="topRight">
+          <div style={{ flexBasis: `${rRepsPercent}%` }} className="republican" title="Republican Representatives">{totalRReps}</div>
+        </Popover>
       </div>
     </div>
   );
