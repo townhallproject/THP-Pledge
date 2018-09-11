4af303b2ce5606ad886ded3ef59cce1ab2fa0b43
meganrm <meganrm@gmail.com>
2018-09-10 23:56:12 -0700

    added banner

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 
+import { DYJD_COLOR, PLEDGED_COLOR } from '../constants';
 import SignPledge from '../SignPledge';
 
 /* eslint-disable */
@@ -29,7 +30,14 @@ class Header extends React.Component {
           <img src={src} alt="banner collage" />
           <div className="spacer"/>
           <div className="background-light text-container">
-            <h1>Take the Town Hall Pledge:</h1>
+          
+            <h1>Accessibility is on the ballot</h1>
+            <p>
+              <strong style={{ color: DYJD_COLOR }}>* 31 * DO YOUR JOB DISTRICTS </strong> 
+              are currently held by incumbent 
+              <strong syle={{ color: 'red'}}> Missing Members </strong> who haven't not held a single open town hall meeting this entire session of Congress running against a challenger who has taken the 
+              <strong style={{ color: PLEDGED_COLOR }}> TOWN HALL PLEDGE</strong>. It’s time to <span style={{ color: DYJD_COLOR }}>Do Your Job*... </span>or let someone else do it for you.</p>
+            <h2>Take the Town Hall Pledge</h2>
             <p>Across party lines, voters in 2018 are demanding accessibility from their lawmakers.
             </p>
             <p>We are asking every single candidate for public office this year to take the <strong>#townhallpledge</strong> to their constituents to hold a minimum of four town hall meetings per year in office.
