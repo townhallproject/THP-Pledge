f458f2b3b7145f5f25dbe697086a5910decf29f8
meganrm <meganrm@gmail.com>
2018-10-11 14:02:31 -0700

    adding popover text

--- src/components/PledgerCell.js
+++ src/components/PledgerCell.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Icon, List } from 'antd';
+import {
+  Icon,
+  List,
+  Avatar,
+  Popover,
+  Badge,
+} from 'antd';
 import { pledgerShape } from '../state/pledgers/types';
+
+/* eslint-disable */
+require('style-loader!css-loader!antd/es/avatar/style/index.css');
+require('style-loader!css-loader!antd/es/tooltip/style/index.css');
+
+/* eslint-enable */
 
 class PledgerCell extends React.Component {
   render() {
@@ -21,16 +33,35 @@ class PledgerCell extends React.Component {
 
     if (!item.displayName) {
       return null;
-    };
+    }
 
     const title = item.incumbent ? (
       <React.Fragment>{item.role.split(' ')[0]}. {item.displayName}* <span className={item.party}>({item.party})</span>
-        {item.missingMember ? <span className="missing-member-icon" /> : null}
       </React.Fragment>) :
       (<React.Fragment>{item.displayName} <span className={item.party}>({item.party})</span></React.Fragment>);
 
-    const description = item.pledged ? (<Icon type="check-circle" />) : (<Icon type="question-circle-o" />);
-   
+    let description = (<Icon type="question-circle-o" />);
+    if (item.missingMember) {
+      // description = (<span className="missing-member-icon" />)
+      description = (
+        <Popover title="Missing Member" content={(
+          <React.Fragment>
+            <p><Avatar size="large" src="/images/missing-member-logo.svg"/>  Has not held a single open town hall this Congressional session.</p>
+          </React.Fragment>
+        )}>
+              <Avatar src="/images/missing-member-logo.svg" />
+        </Popover>
+      );
+    } else if (item.pledged) {
+      description = item.hoverText ? (
+            <Popover content={item.hoverText}>
+              <Badge dot>
+              <Icon type="check-circle" />
+              </Badge>
+            </Popover>
+        ): (<Icon type="check-circle" />);
+    }
+
     return (
       <React.Fragment>
         <List.Item.Meta
