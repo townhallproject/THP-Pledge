67d9a84550b96d711e2c37c4cee98660ac400e9d
Alan Magelssen <alanmagelssen@gmail.com>
2019-03-16 15:05:01 -0700

    update Header container (incl nav)

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button, Icon } from 'antd';
 import { connect } from 'react-redux';
+import Nav from '../../components/Nav';
 
 import DYJDModal from '../../components/DYJD_Modal';
 
@@ -9,6 +11,9 @@ import SignPledge from '../../components/SignPledge';
 
 import { startSetDoYourJobDistricts } from '../../state/do-your-job-district/actions';
 import { totalDoYourJobCount } from '../../state/do-your-job-district/selectors';
+
+import { startSetPledgers } from '../../state/pledgers/actions';
+import { getElectionYear } from '../../state/selections/selectors';
 /* eslint-disable */
 require('style-loader!css-loader!antd/es/button/style/index.css');
 import './style.scss';
@@ -39,20 +44,23 @@ class Header extends React.Component {
     const width = window.innerWidth;
     const src = width > 992 ? 'images/pledgeHeader.jpg' : 'images/pledgeHeader@0,5x.jpg';
     return (
-      <div className="header-container">
+      <div>
+        <Nav startSetPledgers={this.props.startSetPledgers} electionYear={this.props.electionYear} />
+        <div className="header-container">
 
-        <div className="header-pledge--CTA">
-          <img src={src} alt="banner collage" />
-          <div className="spacer" />
-          <div className="background-light text-container">
+          <div className="header-pledge--CTA">
+            <img src={src} alt="banner collage" />
+            <div className="spacer" />
+            <div className="background-light text-container">
 
-            <h2>The Town Hall Pledge</h2>
-            <p>In 2018 we asked every single candidate for public office to take the <strong>#TownHallPledge</strong> to their constituents to hold a minimum of four town hall meetings per year in office.</p>
-            <p>It's never too late to make this common-sense commitment to listen to the people. Ask lawmakers in your community to take the Pledge today!</p>
-            <Button onClick={this.toggleInfo} className="text-right pt-2">Download the pledge</Button>
+              <h2>The Town Hall Pledge</h2>
+              <p>In 2018 we asked every single candidate for public office to take the <strong>#TownHallPledge</strong> to their constituents to hold a minimum of four town hall meetings per year in office.</p>
+              <p>It's never too late to make this common-sense commitment to listen to the people. Ask lawmakers in your community to take the Pledge today!</p>
+              <Button onClick={this.toggleInfo} className="text-right pt-2">Download the pledge</Button>
+            </div>
           </div>
+          {this.state.open ? (<SignPledge />) : null}
         </div>
-        {this.state.open ? (<SignPledge />) : null}
       </div>
     );
   }
@@ -60,10 +68,17 @@ class Header extends React.Component {
 
 const mapStateToProps = state => ({
   allDoYourJobDistricts: totalDoYourJobCount(state),
+  electionYear: getElectionYear(state),
 });
 
 const mapDispatchToProps = dispatch => ({
   getDoYourJobDistricts: () => dispatch(startSetDoYourJobDistricts()),
+  startSetPledgers: year => dispatch(startSetPledgers(year)),
 });
+
+Header.propTypes = {
+  electionYear: PropTypes.string.isRequired,
+  startSetPledgers: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
