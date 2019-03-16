4c739a765fb02a8cd78a30f4bf1599ba7871b98b
Alex <amarvick94@gmail.com>
2019-03-16 13:30:05 -0700

    Dropdown select now retrieves JSON data on change

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,38 @@
 
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Icon, Button, Menu, Dropdown, message } from 'antd';
 
 import './style.scss';
+
+import { startSetPledgers } from '../../state/pledgers/actions'
 
 require('style-loader!css-loader!antd/es/dropdown/style/index.css');
 require('style-loader!css-loader!antd/es/message/style/index.css');
 require('style-loader!css-loader!antd/es/menu/style/index.css');
 
-function handleMenuClick(e) {
-  console.log('click', e.key);
-}
-
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="2018">2018</Menu.Item>
-    <Menu.Item key="2019">2019</Menu.Item>
-  </Menu>
-);
-
 class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChangeYear = this.handleChangeYear.bind(this)
+    this.renderMenu = this.renderMenu.bind(this)
+  }
+
+  handleChangeYear(e) {
+    console.log('click', e.key);
+    this.props.startSetPledgers(e.key)
+  }
+
+  renderMenu() {
+    return (
+      <Menu onClick={this.handleChangeYear}>
+        <Menu.Item key="2018">2018</Menu.Item>
+        <Menu.Item key="2019">2019</Menu.Item>
+      </Menu>
+    );
+  }
+  
   render() {
     return (
       <nav className="navbar navbar-light justify-content-between">
@@ -33,7 +46,7 @@ class Nav extends React.Component {
           <li><Button href="https://secure.actblue.com/contribute/page/townhallproject" className="linkOut btn" id="donate-button" role="button" target="_blank">Donate</Button></li>
           <li><Button href="//townhallproject.com" rel="noopener noreferrer" target="_blank" className="linkOut">Main site <div className="main-site" /></Button></li>
           <li>
-            <Dropdown overlay={menu}>
+            <Dropdown overlay={this.renderMenu()}>
               <Button style={{ marginLeft: 8 }}>
                 Election Year <Icon type="down" />
               </Button>
@@ -45,4 +58,14 @@ class Nav extends React.Component {
   }
 }
 
-export default Nav;
+const mapStateToProps = (state) => {}
+
+const mapDispatchToProps = dispatch => ({
+  startSetPledgers: (year) => dispatch(startSetPledgers(year))
+});
+
+Nav.propTypes = {
+  startSetPledgers: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
